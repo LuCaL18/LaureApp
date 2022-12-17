d520315e01f49a14585021bb75c634681fee231d
LuCaL18 <lucalomuscio2002@gmail.com>
2022-12-17 12:25:35 +0100

    Initial commit

---
+++ app/src/main/java/com/uniba/mobile/cddgl/laureapp/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package com.uniba.mobile.cddgl.laureapp.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
