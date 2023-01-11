eb470dcb172bcf8b374e5e166d666e114001f9e4
pask2504 <105316105+pask2504@users.noreply.github.com>
2023-01-11 15:26:19 +0100

    Nona commit (provvisoria)

--- app/src/main/java/com/uniba/mobile/cddgl/laureapp/ui/task/NewTaskFragment.java
+++ app/src/main/java/com/uniba/mobile/cddgl/laureapp/ui/task/NewTaskFragment.java
@@ -1,14 +1,9 @@
 package com.uniba.mobile.cddgl.laureapp.ui.task;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
@@ -18,83 +13,55 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.MenuProvider;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.NavController;
-import androidx.navigation.fragment.NavHostFragment;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 import com.uniba.mobile.cddgl.laureapp.R;
-import com.uniba.mobile.cddgl.laureapp.data.model.NewTaskIn;
 import com.uniba.mobile.cddgl.laureapp.databinding.FragmentNewTaskBinding;
 
 public class NewTaskFragment extends Fragment {
 
+    private com.uniba.mobile.cddgl.laureapp.ui.task.NewTaskViewModel newTaskViewModel;
     private FragmentNewTaskBinding binding;
-    private BottomNavigationView navBar;
-    private MenuProvider provider;
-    private Toolbar toolbar;
-    private Drawable iconPre;
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    private EditText nometaskEditText;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
-                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
 
         binding = FragmentNewTaskBinding.inflate(inflater, container, false);
         return binding.getRoot();
+
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        NewTaskViewModel newtaskViewModel = new ViewModelProvider(requireActivity()).get(NewTaskViewModel.class);
+        newTaskViewModel = new ViewModelProvider(requireActivity()).get(NewTaskViewModel.class);
 
         final EditText nometaskEditText = binding.nometask;
         final EditText statoEditText = binding.stato;
         final EditText descrizioneEditText = binding.descrizione;
-        final Button addtaskButton = binding.button;
+        final Button addTaskButton = binding.button;
+        final TextView nometaskTesto = binding.nometaskTesto;
+        final TextView statoTesto = binding.statoTesto;
+        final TextView descrizioneTesto = binding.descrizioneTesto;
 
-        newtaskViewModel.getnewtaskFormState().observe(getViewLifecycleOwner(), new Observer<NewTaskFormState>() {
-            @Override
-            public void onChanged(@Nullable NewTaskFormState newTaskFormState) {
-                if (newTaskFormState == null) {
-                    return;
-                }
-                addtaskButton.setEnabled(newTaskFormState.isDataValid());
-                if (newTaskFormState.getNometaskError() != null) {
-                    nometaskEditText.setError(getString(newTaskFormState.getNometaskError()));
-                }
-                if (newTaskFormState.getStatoError() != null) {
-                    statoEditText.setError(getString(newTaskFormState.getStatoError()));
-                }
+        newTaskViewModel.getNewTaskFormState().observe(getViewLifecycleOwner(), newtaskFormState -> {
+            if (newtaskFormState == null) {
+                return;
             }
+            addTaskButton.setEnabled(newtaskFormState.isDataValid());
         });
 
-        /* newtaskViewModel.getnewtaskResult().observe(getViewLifecycleOwner(), new Observer<newtaskResult>() {
-            @Override
-            public void onChanged(@Nullable NewTaskResult newtaskResult) {
-                if (newtaskResult == null) {
-                    return;
-                }
-                if (newtaskResult.getError() != null) {
-                    showNewTaskFailure(newtaskResult.getError());
-                }
-                if (newtaskResult.getSuccess() != null) {
-                    NewTaskIn result = newtaskResult.getSuccess();
-                    updateUIwithUser(result.getNomeTask());
-                    newtaskViewModel.setLoggedUser(result);
-                }
+        newTaskViewModel.getNewTaskResult().observe(getViewLifecycleOwner(), newtaskResult -> {
+            if (newtaskResult == null) {
+                return;
             }
-        }); */
+        });
 
         TextWatcher afterTextChangedListener = new TextWatcher() {
             @Override
@@ -109,42 +76,30 @@ public class NewTaskFragment extends Fragment {
 
             @Override
             public void afterTextChanged(Editable s) {
-                newtaskViewModel.newtaskDataChanged(nometaskEditText.getText().toString(),
+                newTaskViewModel.newtaskDataChanged(nometaskEditText.getText().toString(),
                         statoEditText.getText().toString(),descrizioneEditText.getText().toString());
             }
         };
         nometaskEditText.addTextChangedListener(afterTextChangedListener);
         statoEditText.addTextChangedListener(afterTextChangedListener);
-        statoEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    newtaskViewModel.insertNewTask(nometaskEditText.getText().toString(),
-                            statoEditText.getText().toString(),descrizioneEditText.getText().toString());
-                }
-                return false;
-            }
-        });
-
-        addtaskButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                newtaskViewModel.insertNewTask(nometaskEditText.getText().toString(),
+        descrizioneEditText.addTextChangedListener(afterTextChangedListener);
+        descrizioneEditText.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                newTaskViewModel.newtask(nometaskEditText.getText().toString(),
                         statoEditText.getText().toString(),descrizioneEditText.getText().toString());
             }
+            return false;
+        });
+
+        addTaskButton.setOnClickListener(v -> {
+            newTaskViewModel.newtask(nometaskEditText.getText().toString(),
+                    statoEditText.getText().toString(),descrizioneEditText.getText().toString());
         });
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        navBar.setVisibility(View.VISIBLE);
-        requireActivity().removeMenuProvider(provider);
-        toolbar.setNavigationIcon(iconPre);
-        navBar = null;
         binding = null;
-        provider = null;
-        toolbar = null;
     }
 }
--- app/src/main/java/com/uniba/mobile/cddgl/laureapp/ui/task/NewTaskViewModel.java
+++ app/src/main/java/com/uniba/mobile/cddgl/laureapp/ui/task/NewTaskViewModel.java
@@ -1,13 +1,12 @@
 package com.uniba.mobile.cddgl.laureapp.ui.task;
 
-import androidx.annotation.NonNull;
+import android.util.Patterns;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
@@ -15,11 +14,21 @@ import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.uniba.mobile.cddgl.laureapp.R;
 import com.uniba.mobile.cddgl.laureapp.data.RoleUser;
 import com.uniba.mobile.cddgl.laureapp.data.model.LoggedInUser;
-import com.uniba.mobile.cddgl.laureapp.R;
 import com.uniba.mobile.cddgl.laureapp.data.model.NewTaskIn;
+import com.uniba.mobile.cddgl.laureapp.ui.login.LoginFormState;
+import com.uniba.mobile.cddgl.laureapp.ui.login.LoginResult;
+import com.uniba.mobile.cddgl.laureapp.ui.login.registration.RegisterFormState;
 
+import org.w3c.dom.Document;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -27,25 +36,19 @@ public class NewTaskViewModel extends ViewModel {
 
     private final MutableLiveData<NewTaskFormState> newtaskFormState = new MutableLiveData<>();
     private final MutableLiveData<NewTaskResult> newtaskResult = new MutableLiveData<>();
-    // private final MutableLiveData<LoggedInUser> loggedUser = new MutableLiveData<>(); //
-    private FirebaseAuth auth;
+    private final MutableLiveData<NewTaskIn> newtask = new MutableLiveData<>();
+    private final FirebaseFirestore db;
+    private NewTaskIn task;
 
     NewTaskViewModel() {
-        auth = FirebaseAuth.getInstance();
-
-        //TODO gestire la logout nel navigation view del main activity
-        auth.signOut();
-
-        FirebaseUser task = auth.getCurrentUser();
-
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
     }
 
-    LiveData<NewTaskFormState> getnewtaskFormState() {
+    public MutableLiveData<NewTaskFormState> getNewTaskFormState() {
         return newtaskFormState;
     }
 
-
-    public LiveData<NewTaskResult> getnewtaskResult() {
+    public MutableLiveData<NewTaskResult> getNewTaskResult() {
         return newtaskResult;
     }
 
@@ -53,95 +56,76 @@ public class NewTaskViewModel extends ViewModel {
         return loggedUser;
     } */
 
-    public void insertNewTask(String nomeTask, String stato, String descrizione) {
-        auth.createUserWithEmailAndPassword(nomeTask,stato).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-
-                if (task.isSuccessful()) {
-                    FirebaseUser user = auth.getCurrentUser();
-
-                    NewTaskIn newtaskIn = new NewTaskIn(nomeTask, stato, descrizione, RoleUser.STUDENT);
-
-                    FirebaseDatabase database = FirebaseDatabase.getInstance();
-                    DatabaseReference myRef = database.getReference("newtask");
-
-                    myRef.child(user.getUid()).setValue(newtaskIn).addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            if (task.isSuccessful()) {
-                                newtaskResult.setValue(new NewTaskResult(newtaskIn));
-                            } else {
-                                newtaskResult.setValue(new NewTaskResult(R.string.invalid_state));
-                            }
-                        }
-                    });
-
-                } else {
-
-                    try {
-                        throw task.getException();
-                    } catch (FirebaseAuthWeakPasswordException e) {
-                        newtaskResult.setValue(new NewTaskResult(R.string.invalid_credentials));
-                    } catch (FirebaseAuthInvalidCredentialsException e) {
-                        newtaskResult.setValue(new NewTaskResult(R.string.invalid_credentials));
-                    } catch (FirebaseAuthUserCollisionException e) {
-                        newtaskResult.setValue(new NewTaskResult(R.string.invalid_credentials));
-                    } catch (Exception e) {
-                        newtaskResult.setValue(new NewTaskResult(R.string.invalid_credentials));
-                    }
-                }
-            }
-        });
+    public MutableLiveData<NewTaskIn> getNewTask() {
+        return newtask;
     }
 
-    public void newtaskDataChanged(String nomeTask, String stato, String descrizione) {
-        if (!isTaskNameValid(nomeTask)) {
+    public void newtask(String nometask, String stato, String descrizione) {
+
+        auth.signInWithEmailAndPassword(nometask,stato,descrizione)
+                .addOnCompleteListener(task -> {
+
+                    if (task.isSuccessful()) {
+                        FirebaseUser task = auth.get();
+                        newtaskResult.setValue(new LoginResult(new LoggedInUser(nometask, task.getDisplayName())));
+                    } else {
+                        newtaskResult.setValue(new LoginResult(R.string.invalid_task));
+                    }
+                });
+    }
+
+    public void newtaskDataChanged(String nometask, String stato, String descrizione) {
+        if (!isNameTaskValid(nometask)) {
             newtaskFormState.setValue(new NewTaskFormState(R.string.invalid_taskname, null, null));
-        } else if (!isStatoValid(stato)) {
+        } else if (!isStateValid(stato)) {
             newtaskFormState.setValue(new NewTaskFormState(null, R.string.invalid_state, null));
         } else if (!isDescriptionValid(descrizione)) {
-            newtaskFormState.setValue(new NewTaskFormState(null, null, R.string.invalid_description));
+            newtaskFormState.setValue(new NewTaskFormState( null, null, R.string.invalid_description));
         } else {
             newtaskFormState.setValue(new NewTaskFormState(true));
         }
     }
 
-    /*
-    public void setLoggedUser(LoggedInUser user) {
-        this.loggedUser.setValue(user);
-    } */
+    public void confirmNewTask(String id) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("task");
 
-    // A placeholder task name and state validation check
-    private boolean isTaskNameValid(String nometask) {
-        if (nometask == null) {
+        myRef.child(id).setValue(task).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                newtaskResult.setValue(new NewTaskResult(task));
+            } else {
+                newtaskResult.setValue(new NewTaskResult(R.string.invalid_task));
+            }
+        });
+    }
+
+    public void setTask (NewTaskIn task) {
+        this.loggedUser.setValue(task);
+    }
+
+    // A placeholder name and surname validation check
+    private boolean isNameTaskValid(String nometask) {
+        if (nometask == null || nometask.isEmpty()) {
             return false;
         }
+
         String strPattern = "[~!@#$%^&*()_+{}\\[\\]:;,.<>/?-]";
         Pattern p = Pattern.compile(strPattern);
         Matcher m = p.matcher(nometask);
-        if (m.find()) {
-            return false;
-        }
-        return true;
+        return !m.find();
     }
 
-    private boolean isStatoValid(String stato) {
-        if (stato == null) {
-            return false;
-        }
-        String strPattern = "[~!@#$%^&*()_+{}\\[\\]:;,.<>/?-]";
-        Pattern p = Pattern.compile(strPattern);
-        Matcher m = p.matcher(stato);
-        if (m.find()) {
-            return false;
-        }
-        return true;
-    }
-
-    // La descrizione deve avere massimo 200 caratteri
     private boolean isDescriptionValid(String descrizione) {
         return descrizione.split("").length < 200;
+    }
+
+    // A placeholder password validation check
+    private boolean isStateValid(String stato) {
+        return stato != null && stato.trim().length() > 5;
+    }
+
+    public LoggedInUser getCurrentLoggedUser() {
+        return currentLoggedUser;
     }
 
 }
