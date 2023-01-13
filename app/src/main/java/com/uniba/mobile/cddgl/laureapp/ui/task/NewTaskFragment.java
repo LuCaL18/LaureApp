a0f66716329ec22490fa2796a9df6f2c7ae20772
pask2504 <105316105+pask2504@users.noreply.github.com>
2023-01-13 17:15:08 +0100

    Quattordicesima commit (ottima ottima)

--- app/src/main/java/com/uniba/mobile/cddgl/laureapp/ui/task/NewTaskFragment.java
+++ app/src/main/java/com/uniba/mobile/cddgl/laureapp/ui/task/NewTaskFragment.java
@@ -7,30 +7,30 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.uniba.mobile.cddgl.laureapp.R;
 import com.uniba.mobile.cddgl.laureapp.data.model.Task;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class NewTaskFragment extends Fragment {
 
-    DatabaseReference databaseReference;
-    FirebaseDatabase firebaseDatabase;
-    EditText nometaskEditText,statoEditText,descrizioneEditText;
-    Button addtaskButton;
+    private FirebaseFirestore db;
+    private EditText nometaskEditText,statoEditText,descrizioneEditText;
+    private Button addtaskButton;
     private OnFragmentInteractionListener listener;
 
     public NewTaskFragment() {
-
+        //
     }
 
     public static NewTaskFragment newInstance() {
@@ -45,40 +45,35 @@ public class NewTaskFragment extends Fragment {
     @Override
     public View onCreateView (@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_new_task,container,false);
-        firebaseDatabase = FirebaseDatabase.getInstance("https://laureapp-b5243-default-rtdb.firebaseio.com/");
+        db = FirebaseFirestore.getInstance();
         nometaskEditText = view.findViewById(R.id.nometask);
         statoEditText = view.findViewById(R.id.stato);
         descrizioneEditText = view.findViewById(R.id.descrizione);
         addtaskButton = view.findViewById(R.id.addtask_button);
 
-        addtaskButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick (View v) {
-                String nometask = nometaskEditText.getText().toString();
-                String stato = statoEditText.getText().toString();
-                String descrizione = descrizioneEditText.getText().toString();
-                Task task = new Task(nometask,stato,descrizione);
-                listener.onAddTaskClicked(task);
-            }
-        });
         addtaskButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick (View view) {
-                Task task = new Task();
-                // List<Task> listaTask = new ArrayList<>();
-                task.setNometask(nometaskEditText.getText().toString());
-                task.setStato(statoEditText.getText().toString());
-                task.setDescrizione(descrizioneEditText.getText().toString());
-                databaseReference = firebaseDatabase.getReference("task");
-                databaseReference.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        databaseReference.setValue(task);
-                    }
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-                    }
-                });
+                String nometask = nometaskEditText.getText().toString();
+                String stato = statoEditText.getText().toString();
+                String descrizione = descrizioneEditText.getText().toString();
+                Map<String,Object> listaTask = new HashMap<>();
+                listaTask.put("nomeTask",nometask);
+                listaTask.put("stato",stato);
+                listaTask.put("descrizione",descrizione);
+                db.collection("task")
+                        .add(listaTask)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Toast.makeText(getContext(), "successfull", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getContext(), "failed", Toast.LENGTH_SHORT).show();
+                            }
+                        });
             }
         });
         return view;
