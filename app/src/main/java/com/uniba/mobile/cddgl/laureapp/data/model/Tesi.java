ac02cb69c6f14f77388e504669ccad91a64df5df
pask2504 <105316105+pask2504@users.noreply.github.com>
2023-01-21 11:50:46 +0100

    Aggiunta lista tesi adapter

--- app/src/main/java/com/uniba/mobile/cddgl/laureapp/data/model/Tesi.java
+++ app/src/main/java/com/uniba/mobile/cddgl/laureapp/data/model/Tesi.java
@@ -3,21 +3,35 @@ package com.uniba.mobile.cddgl.laureapp.data.model;
 public class Tesi {
 
     private String nomeTesi;
+    private String relatore;
     private String[] relatori;
     private String[] vincoli;
     private Task[] task;
     private String nomeStudente;
 
-    public Tesi (String nomeTesi, String[] relatori, String[] vincoli, Task[] task, String nomeStudente) {
+    public Tesi (String nomeTesi, String relatore) {
         this.nomeTesi = nomeTesi;
+        this.relatore = relatore;
+    }
+
+    public Tesi (String nomeTesi, String relatore, String nomeStudente) {
+        this.nomeTesi = nomeTesi;
+        this.relatore = relatore;
+        this.nomeStudente = nomeStudente;
+    }
+
+    public Tesi (String nomeTesi, String relatore, String[] relatori, String[] vincoli, Task[] task, String nomeStudente) {
+        this.nomeTesi = nomeTesi;
+        this.relatore = relatore;
         this.relatori = relatori;
         this.vincoli = vincoli;
         this.task = task;
         this.nomeStudente = nomeStudente;
     }
 
-    public Tesi (String nomeTesi, String[] relatori, String[] vincoli, String nomeStudente) {
+    public Tesi (String nomeTesi, String relatore, String[] relatori, String[] vincoli, String nomeStudente) {
         this.nomeTesi = nomeTesi;
+        this.relatore = relatore;
         this.relatori = relatori;
         this.vincoli = vincoli;
         this.nomeStudente = nomeStudente;
@@ -45,6 +59,14 @@ public class Tesi {
 
     public void setRelatori(String[] relatori) {
         this.relatori = relatori;
+    }
+
+    public String getRelatore() {
+        return relatore;
+    }
+
+    public void setRelatore(String relatore) {
+        this.relatore = relatore;
     }
 
     public String getNomeTesi() {
--- app/src/main/java/com/uniba/mobile/cddgl/laureapp/ui/tesi/ListaTesiFragment.java
+++ app/src/main/java/com/uniba/mobile/cddgl/laureapp/ui/tesi/ListaTesiFragment.java
@@ -15,20 +15,22 @@ import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.firebase.ui.database.FirebaseListOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.uniba.mobile.cddgl.laureapp.R;
+import com.uniba.mobile.cddgl.laureapp.data.model.Tesi;
 
 public class ListaTesiFragment extends Fragment {
 
-    private FirebaseFirestore db;
-    private TextView nomeTesiView;
-    private TextView relatoreView;
-    private ImageButton visualizzaButton;
-    private ImageButton addButton;
+    ListView listview;
+    FirebaseListAdapter adapter;
 
     public ListaTesiFragment() {
         //
@@ -47,8 +49,37 @@ public class ListaTesiFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_lista_tesi,container,false);
-        ListView listView = view.findViewById(R.id.listatesi);
+        Query query = FirebaseDatabase.getInstance().getReference().child("tesi");
+        listview = (ListView) findViewById(R.id.listatesi);
+        FirebaseListOptions<Tesi> options = new FirebaseListOptions.Builder<Tesi>()
+                .setLayout(R.layout.lista_tesi)
+                .setLifecycleOwner(start.this)
+                .setQuery(query, Tesi.class)
+                .build();
+        adapter = new FirebaseListAdapter(options) {
+            @Override
+            protected void populateView(@NonNull View v, @NonNull Object model, int position) {
+                TextView nomeTesi = v.findViewById(R.id.nometesi2);
+                TextView relatoreTesi = v.findViewById(R.id.nomerelatore2);
+                ImageButton visualizzaTesi;
+                ImageButton addTesi;
+                Tesi tesi = (Tesi) model;
+                nomeTesi.getText(tesi.getNomeTesi().toString());
+                relatoreTesi.getText(tesi.getRelatore().toString());
+            }
+        };
+        listview.setAdapter(adapter);
         return view;
+    }
+
+    public void onStart () {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    public void onStop () {
+        super.onStop();
+        adapter.stopListening();
     }
 
 }
